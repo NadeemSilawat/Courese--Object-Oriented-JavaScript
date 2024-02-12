ed7e76784ad52d2b19bb5536cef71938bfec91e3
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-02-12 16:50:36 +0530

    method-chaining

--- 05-method-chaining.js
+++ 05-method-chaining.js
@@ -22,4 +22,4 @@ class User {
 var userOne = new User('ryu@ninjas.com', 'Ryu');
 var userTwo = new User('yoshi@mariokorp.com', 'Yoshi');
 
-userOne.login().updateScore().updateScore().logout();
+userOne.login().updateScore().updateScore().logout(); 
